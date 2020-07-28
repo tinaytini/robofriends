91d5308c45e4e9aaae0b5a8f358d693be9ba3aff
Tina <tinaytini@yahoo.com>
2020-07-28 11:46:38 +0800

    last commit

--- src/container/App.js
+++ src/container/App.js
@@ -1,5 +1,6 @@
-import React, { Component, useReducer } from 'react';
+import React, { Component } from 'react';
 import CardList from '../components/CardList';
+import ErrorBoundary from '../components/ErrorBoundary';
 import Scroll from '../components/Scroll';
 import SearchButton from '../components/SearchButton';
 import './app.css';
@@ -31,15 +32,18 @@ class App extends Component {
         const searchFilter = robots.filter(robot => {
             return robot.name.toLowerCase().includes(searchValue.toLowerCase())
         })
+
         return !robots.length ?
-        <h1>Loading...</h1> : 
-        <div className='tc'>
-        <h1 className='f1'>RoboFriends</h1>
-        <SearchButton onHandleClick={this.handleClick} />
-        <Scroll>
-            <CardList robotsList={searchFilter} />
-        </Scroll>   
-    </div>   
+            <h1>Loading...</h1> : 
+            <div className='tc'>
+                <h1 className='f1'>RoboFriends</h1>
+                <SearchButton onHandleClick={this.handleClick} />
+                <Scroll>
+                    <ErrorBoundary>
+                        <CardList robotsList={searchFilter} />
+                    </ErrorBoundary>
+                </Scroll>   
+            </div>   
     }
 }
 
